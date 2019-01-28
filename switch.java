d907219caf45217ae99255dd80196cfe4a6231c4
flabbyeric <flabbyeric@gmail.com>
2019-01-28 11:01:24 -0500

    Update switch.java

--- switch.java
+++ switch.java
@@ -1,22 +1,32 @@
+//switch only works with 
+//char, byte, short, int, String, enum  (4 primitive type + String, enum)
+//Character, Byte, Short, Integer
+//boolean is too simply, long/float/double is too complex
+
+//multiple cases case 1: case 2: case:3
+//case 1: 
+//case 2: 
+//case 3:
+
+//case n:   # n has to be constant during the comiling time. 
+//last case: break is optional because the code comes to the end anyway
+
+
 public class Main { 
-   final int x=3;
- 
     public static void main(String[] args) { 
-        int month = 2;
+        int x = 2;
         final int flag=4;
-        String monthString;
-        switch (month) {
-            case 1:          System.out.println("1");
+        switch (x) {
+            case 1:  System.out.println("1");
                      break;
-            case 2:          System.out.println("2");
+            case 2:  System.out.println("2");
                      break;
-            case 3:          System.out.println("3");
-                    break;
-            case flag:       System.out.println("1");
+            case 3:  System.out.println("3");
+                     break;
+            case flag: System.out.println("1");
                     break;
             default:  System.out.println("default");
-            
+                    //break;    
         }
-
       } 
 } 
