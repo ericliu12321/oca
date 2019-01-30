8f0dc3b73cbe488f4e6f8f9aa32a7aab6232a4db
flabbyeric <flabbyeric@gmail.com>
2019-01-30 14:56:52 -0500

    Update date.java

--- date.java
+++ date.java
@@ -20,8 +20,35 @@ public class Gosling {
     System.out.println(date.getDayOfYear());
     System.out.println(date.getMonth());
     System.out.println(date.getMonthValue());
-    System.out.println(date.getYear());    
+    System.out.println(date.getYear());   
     
+    
+    LocalDate JohnBday=LocalDate.parse("2002-08-30");
+    LocalDate SaraBday=LocalDate.parse("2002-07-29");
+    System.out.println(JohnBday.isAfter(SaraBday));
+    System.out.println(JohnBday.isBefore(SaraBday));
+    
+    //LocalDate is immutable like String
+    //Thrown DateTimeParseException
+    LocalDate bday = LocalDate.of (2052,03,10);
+    System. out .println(bday.minusDays(10));
+    System.out.println(bday.minusMonths(2));
+    System.out .println(bday.minusWeeks(30));
+    System.out .println(bday.minusYears(1));
+
+    System. out .println(bday.plusDays(10));
+    System.out.println(bday.plusMonths(2));
+    System.out .println(bday.plusWeeks(30));
+    System.out .println(bday.plusYears(1));    
+
+
+    LocalDate firstDay= LocalDate.of(2036,02,28) ;
+    System.out.println(firstDay.withDayOfMonth(1));
+    System.out.println(firstDay.withDayOfYear(1));
+    System.out.println(firstDay.withMonth(7));
+    System.out.println(firstDay.withYear(1));
+   
+
   }
 }
 
