1f3875a7af3267e7f7675b24db67dde7ba1d6261
flabbyeric <flabbyeric@gmail.com>
2019-01-30 18:07:37 -0500

    Update exception.java

--- exception.java
+++ exception.java
@@ -4,6 +4,29 @@ Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 8
 	at GFG.method1(File.java:10)
 	at GFG.main(File.java:7)
 */
+/*
+  Checked Exception: java.lang.Exception other than java.lang.RuntimeException)
+  public FileInputStream(File file) throws FileNotFoundException
+  
+  Runtime exception: subclass of java.lang.RuntimeException
+    ArraylndexOutOfBoundsExcept ion
+    IndexOutOfBoundsExcept ion
+    ClassCastException
+    IllegalArgumentException
+    ArithmeticException
+    NullPointerException
+    NumberFormatException
+  
+  Error:  java.lang.Error
+    StackOverf lowError
+    NoClassDefFoundError
+    OutOfMemoryError
+    ExceptionlnlnitializerError    
+
+
+ ArraylndexOutOfBoundsException: when access array 
+ IndexOutOfBoundsException: When access ArrayList 
+*/
 
 import java.io.*;
 
@@ -19,3 +42,17 @@ class GFG {
 	    System.out.println(s[8]);
 	}
 }
+
+
+import java. util .ArrayList;
+public class ListAccess {
+   public static void main(String args[1) {
+      ArrayList<Ink> inks = new ArrayList<Ink>();
+      inks.add(new Colorlnk());
+      inks.add(new Blacklnk());
+      Ink ink = (Blacklnk) inks.get(O) ;  //Throws ClassCastException, you can check with instanceof 
+    }
+}
+class Ink{}
+class Colorlnk extends Ink{}
+class Blacklnk extends Ink{}
