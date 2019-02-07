f3478e10bbe266a4daf936c18a7f82747d1d9f6c
flabbyeric <flabbyeric@gmail.com>
2019-02-07 12:33:50 -0500

    Update access_modifier.java

--- access_modifier.java
+++ access_modifier.java
@@ -16,11 +16,39 @@ final method: method cannot be overriden
 *** static method: must have a body {}, cannot be abastract, can only access static var, no polymorph
 
 Variable:
-*** abastract: not applicable (doesn't make sense)
+*** abastract: not applicable in any case 
 final: can be assigned only once (either directly or in the init block or constructor, no regular method)
 static: class variable
 
 */
+
+//example 
+final class Person {}
+class Professor extends Person {}  //wont compile, try to subclass from final class.
+
+final interface Myinterface {}  //wont compile, interface is abastract by nature, so conflict with final
+class Person {
+    final int MAX_AGE;
+    Person() {
+        MAX_AGE=100;  //compile ok. assigned once in the constructor
+    }
+}
+
+class Person {
+    final int MAX_AGE=100;
+    Person() {
+        MAX_AGE=101;  //wont compile. reassigment not allowed
+    }
+}
+
+
+class Person {
+    final void sing() { }
+}
+class Professor extends Person {
+    void sing() {}  //wont compiloe, overriding final method
+}
+
 
 //private constructor: constructor chain, singleton class
 class MySingleton 
@@ -50,8 +78,16 @@ class MySingleton
     }
 } 
 
-
-
+//abstract class: intended to be subclassed, cannot be instantiated
+abstract class Student {
+     String name;
+     public void displayName() {
+     }
+}
+    
+class College {
+    Student s=new Student();  //won't compile, trying to instantiated a abastract class
+}
 
 
 /* static method overridden */
