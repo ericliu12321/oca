9ac414bcdf040e72b31ba7a2f5baae42d08bd3b4
flabbyeric <flabbyeric@gmail.com>
2019-02-07 15:18:50 -0500

    Update access_modifier.java

--- access_modifier.java
+++ access_modifier.java
@@ -25,7 +25,7 @@ static: class variable (can only access static method)
 /* interface 
 interface: public or default
 method: public abstract (implicitly)
-variable: public static final
+variable: public static final (implicitly)
 */
 
 
